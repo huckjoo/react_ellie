58f53beb68a482c6acdd1ca975f23fb1d0063cae
huckjoo <omp0109@naver.com>
2022-12-17 23:12:05 +0900

    docs: ~4.6

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { Fragment } from 'react';
 import './App.css';
 
 function App() {
+  const name = '디크론';
+  const list = ['우유', '딸기', '바나나', '메론'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
+      <h2>Hello!</h2>
+      <p>{name}</p>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <img
+        style={{ width: '200px', height: '200px' }}
+        src='https://images.unsplash.com/photo-1660315999670-1d5beab50d96?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1635&q=80'
+        alt='whale'
+      />
+    </>
   );
 }
 
