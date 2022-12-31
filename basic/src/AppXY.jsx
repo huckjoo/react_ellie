0c070e34b7cebc8039c39d877e62a686b2967270
huckjoo <omp0109@naver.com>
2022-12-31 19:43:41 +0900

    lecture: mousepointer solution

--- basic/src/AppXY.jsx
+++ basic/src/AppXY.jsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-  const [mousePosition, setMousePosition] = useState({ left: 0, top: 0 });
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 }); // 상태를 객체 단위로 보관, 업데이트
 
   const handleMouseMove = (e) => {
-    console.log(e.pageX, e.pageY);
-    setMousePosition({ left: e.pageX, top: e.pageY });
+    setMousePosition((prev) => ({ ...prev, x: e.clientX }));
   };
   return (
     <div onMouseMove={handleMouseMove} className='container'>
       <div
-        style={{ left: mousePosition.left - 15, top: mousePosition.top - 15 }}
+        style={{
+          transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
+        }}
         className='pointer'
       ></div>
     </div>
